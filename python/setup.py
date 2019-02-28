f70561f8efbfb0d83a5a7d2b08f71328587eeffd
Pat Buxton <patrick.buxton@tartansolutions.com>
2019-02-28 09:40:03 -0500

    Add k8s dependency

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openfaas-client",
-    install_requires=['requests', 'simplejson', 'urllib3'],
+    install_requires=['requests', 'simplejson', 'urllib3', 'kubernetes'],
     version="0.1.0",
     packages=find_packages(),
     author="Garrett Bates",
